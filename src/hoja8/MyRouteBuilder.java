ef9bbbc534c1afabeb757ef5a0ee5f586491e5fb
Mario Barrientos <mario_b_a03@hotmail.com>
2017-11-05 22:03:33 -0600

    adding jars

--- src/hoja8/MyRouteBuilder.java
+++ src/hoja8/MyRouteBuilder.java
@@ -13,7 +13,7 @@ public class MyRouteBuilder extends org.apache.camel.builder.RouteBuilder{
 
     @Override
     public void configure() throws Exception {
-        from("twitter://search?type=direct&keywords=BigData&consumerKey=HqHOnhDl9dVtvIgH51HHacGby&consumerSecret=eKiRuSFh6SBiEWzbN1ZXagLbJ1OlRmEuS1Cpz9dMn2rZIy8Hvw&accessToken=81978272-46rRm1Uhp6AHZuCNbeEO9vlJN3pKKrZaqNRaQU3P0&accessTokenSecret=xGEk8Hz1EsYSlUwYiz9N1wqsFpvnuw1r1JjJQlY2j0kDD")
+        from("file:C:/Users/mario/Desktop/UVG/10. Decimo Semestre/CC3048 - Programacion y Tecnologias Integradoras/HDT/8/hoja8/input")
                 .process(new MyLogProcessor())
                 .bean(new MyTransformer(), "TransformContent")
                 .to("file:C:/Users/mario/Desktop/UVG/10. Decimo Semestre/CC3048 - Programacion y Tecnologias Integradoras/HDT/8/hoja8/output");
