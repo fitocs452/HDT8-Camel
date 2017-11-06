303483385a0b3d760874fb904df1422fe4d2af98
Adolfo Morales <gadolfomoralesmartinez@gmail.com>
2017-11-06 17:02:44 -0600

    Modify

--- camel-twitter/src/main/java/org/apache/camel/example/websocket/TwitterWebSocketRoute.java
+++ camel-twitter/src/main/java/org/apache/camel/example/websocket/TwitterWebSocketRoute.java
@@ -6,10 +6,6 @@ import org.apache.camel.component.websocket.WebsocketComponent;
 import java.io.FileWriter;
 import java.io.IOException;
 
-/**
- * A Camel route that updates from twitter all tweets using having the search term.
- * And post the changes to web-socket, that can be viewed from a web page
- */
 public class TwitterWebSocketRoute extends RouteBuilder {
 
     private int port = 9090;
@@ -20,56 +16,28 @@ public class TwitterWebSocketRoute extends RouteBuilder {
     private String accessToken;
     private String accessTokenSecret;
 
-    public String getAccessToken() {
-        return accessToken;
-    }
-
     public void setAccessToken(String accessToken) {
         this.accessToken = accessToken;
-    }
-
-    public String getAccessTokenSecret() {
-        return accessTokenSecret;
     }
 
     public void setAccessTokenSecret(String accessTokenSecret) {
         this.accessTokenSecret = accessTokenSecret;
     }
 
-    public String getConsumerKey() {
-        return consumerKey;
-    }
-
     public void setConsumerKey(String consumerKey) {
         this.consumerKey = consumerKey;
-    }
-
-    public String getConsumerSecret() {
-        return consumerSecret;
     }
 
     public void setConsumerSecret(String consumerSecret) {
         this.consumerSecret = consumerSecret;
     }
 
-    public int getDelay() {
-        return delay;
-    }
-
     public void setDelay(int delay) {
         this.delay = delay;
     }
 
-    public String getSearchTerm() {
-        return searchTerm;
-    }
-
     public void setSearchTerm(String searchTerm) {
         this.searchTerm = searchTerm;
-    }
-
-    public int getPort() {
-        return port;
     }
 
     public void setPort(int port) {
@@ -95,8 +63,7 @@ public class TwitterWebSocketRoute extends RouteBuilder {
         //Stream from twitter
         fromF("twitter-search://%s?delay=%s", searchTerm, delay)
             .to("log:tweet")
-            .to("websocket:camel-tweet?sendToAll=true")
-			.to("twitter:timeline/user");
+            .to("websocket:camel-tweet?sendToAll=true");
 
         // try {
         //     FileWriter writer = new FileWriter("MyFile.txt", true);
